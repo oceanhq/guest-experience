27bca64989633eb51a4c4b3ac13c41ab6f6dd41e
Toby Sullivan <tobyjsullivan@gmail.com>
2016-05-18 23:25:45 -0700

    Add simple listing and creating streams

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var Streams = require('../lib/streams.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  pStreams = Streams.ListStreams();
+  pStreams.then(function(streams) {
+    res.render('index', { title: 'Ocean', streams: streams });
+  })
+  .catch(function(error) {
+    res.render('error', {message: error, error: error});
+  }) ;
+
+
+});
+
+router.post('/create-stream', function(req, res, next) {
+  if(!req.is('application/json')) {
+    res.status(400).send({ status: 'fail', error: 'Request must be JSON'});
+    return;
+  }
+
+  if(req.body.name === undefined) {
+    res.status(400).send({ status: 'fail', error: 'No name'});
+    return;
+  }
+
+  Streams.CreateStream(req.body.name)
+  .then(function(stream) {
+    res.send({ status: 'ok' });
+  });
 });
 
 module.exports = router;
