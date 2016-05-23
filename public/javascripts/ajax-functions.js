4f16e77a3f5e66f5699fbe07d0c9a69c5c321d14
Toby Sullivan <tobyjsullivan@gmail.com>
2016-05-22 21:36:18 -0700

    Use strict

--- public/javascripts/ajax-functions.js
+++ public/javascripts/ajax-functions.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function postCreateStream(streamName, cbSuccess, cbFailure) {
   var data = { name: streamName };
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+'use strict'
+
 var express = require('express');
 var Streams = require('../lib/streams.js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  pStreams = Streams.ListStreams();
+  var pStreams = Streams.ListStreams();
   pStreams.then(function(streams) {
     res.render('index', { title: 'Ocean', streams: streams });
   })
