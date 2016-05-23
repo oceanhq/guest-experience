82a18cc947a43ca374b41e91089693ccdba0163a
Toby Sullivan <tobyjsullivan@gmail.com>
2016-05-22 22:00:32 -0700

    Add some debug logging

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log('DEBUG: Received \'/\'');
+  console.log('DEBUG: Fetching list of streams.');
   var pStreams = Streams.ListStreams();
   pStreams.then(function(streams) {
+    console.log('DEBUG: Successful response.');
     res.render('index', { title: 'Ocean', streams: streams });
+    console.log('DEBUG: Done rendering index.')
   })
   .catch(function(errorRes) {
+    console.log('WARN: Error response. '+JSON.stringify(errorRes));
     error = errorRes.error;
     res.render('error', {message: error.message, error: JSON.stringify(error.service.error)});
+    console.log('DEBUG: Done rendering error.');
   });
 
 });
