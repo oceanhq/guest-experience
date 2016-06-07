4d12d46f9b325353d4e54c63ebbcc2517e909510
Toby Sullivan <tobyjsullivan@gmail.com>
2016-06-06 22:00:00 -0700

    Add beginnings of Get Started flow

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
 
   res.render('index', {});
+});
+
+router.get('/get-started', function(req, res) {
+
+  res.render('get-started', {});
 });
 
 module.exports = router;
